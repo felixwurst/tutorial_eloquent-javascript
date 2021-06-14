8da57ea2dfdc14e7382a442e4f689b91e8fa4406
felixwurst <felix.wurst@gmail.com>
2021-06-14 19:38:26 +0200

    optimised solution

--- 11_asynchronous-programming/solutions/solution11_2.js
+++ 11_asynchronous-programming/solutions/solution11_2.js
@@ -29,12 +29,12 @@ function Promise_all(promises) {
 
 // Test code
 
-// empty array
+// Empty array
 Promise_all([]).then(array => {
   console.log('This should be []:', array);
 });
 
-// order array
+// Order array
 function soon(val) {
   return new Promise(resolve => {
     setTimeout(() => resolve(val), Math.random() * 500);
@@ -45,7 +45,7 @@ Promise_all([soon(1), soon(2), soon(3)]).then(array => {
   console.log('This should be [1, 2, 3]:', array);
 });
 
-// error handling
+// Handle error
 Promise_all([soon(1), Promise.reject('X'), soon(3)])
   .then(array => {
     console.log('We should not get here');
