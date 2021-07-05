a4f09653bee8eee8ca6703461b4260ab86be115c
felixwurst <felix.wurst@gmail.com>
2021-07-05 22:33:40 +0200

    started to solve the task

--- 12_project_programming-language/solutions/solution12_4.js
+++ 12_project_programming-language/solutions/solution12_4.js
@@ -7,12 +7,42 @@ This ambiguity causes a problem. When you try to give a nonlocal binding a new v
 
 Add a special form set, similar to define, which gives a binding a new value, updating the binding in an outer scope if it doesn’t already exist in the inner scope. If the binding is not defined at all, throw a ReferenceError (another standard error type).
 
-The technique of representing scopes as simple objects, which has made things convenient so far, will get in your way a little at this point. You might want to use the Object.getPrototypeOf function, which returns the prototype of an object. Also remember that scopes do not derive from Object.prototype, so if you want to call hasOwnProperty on them, you have to use this clumsy expression:
-Object.prototype.hasOwnProperty.call(scope, name);
+The technique of representing scopes as simple objects, which has made things convenient so far, will get in your way a little at this point. You might want to use the Object.getPrototypeOf function, which returns the prototype of an object. Also remember that scopes do not derive from Object.prototype, so if you want to call hasOwnProperty on them, you have to use this clumsy expression: Object.prototype.hasOwnProperty.call(scope, name);
 */
 
+// load dependencies
+require('../code/load')('code/chapter/12_language.js');
+
+// specialForms.define = (args, scope) => {
+//   if (args.length != 2 || args[0].type != 'word') {
+//     throw new SyntaxError('Incorrect use of define');
+//   }
+//   let value = evaluate(args[1], scope);
+//   scope[args[0].name] = value;
+//   return value;
+// };
+
 specialForms.set = (args, scope) => {
-  // Your code here.
+  // outer scope: { x: 4, setx: [Function] }
+  // inner scope: { val: 50 }
+  if (Object.prototype.hasOwnProperty.call(scope, args[0].name)) {
+    // overwrite x
+    console.log('arg in scope');
+    // x
+    console.log(args[0]);
+    // val
+    console.log(args[1]);
+    // let value = evaluate(args[1], scope);
+    // console.log(value);
+    // scope[args[0].name] = value;
+    // return value;
+  }
+  if (Object.getPrototypeOf(scope) == null) {
+    console.log('error');
+  } else {
+    console.log('recursive');
+    specialForms.set(args, Object.getPrototypeOf(scope));
+  }
 };
 
 run(`
@@ -22,5 +52,5 @@ do(define(x, 4),
    print(x))
 `);
 // → 50
-run(`set(quux, true)`);
+// run(`set(quux, true)`);
 // → Some kind of ReferenceError
