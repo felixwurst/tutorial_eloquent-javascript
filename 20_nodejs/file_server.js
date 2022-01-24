981c8cd64664259e26b678478c506954641ae6e7
felixwurst <felix.wurst@gmail.com>
2022-01-24 21:22:18 +0100

    formatted

--- 20_nodejs/file_server.js
+++ 20_nodejs/file_server.js
@@ -1,4 +1,4 @@
-const {createServer} = require("http");
+const {createServer} = require('http');
 
 const methods = Object.create(null);
 
@@ -9,65 +9,63 @@ createServer((request, response) => {
       if (error.status != null) return error;
       return {body: String(error), status: 500};
     })
-    .then(({body, status = 200, type = "text/plain"}) => {
-       response.writeHead(status, {"Content-Type": type});
-       if (body && body.pipe) body.pipe(response);
-       else response.end(body);
+    .then(({body, status = 200, type = 'text/plain'}) => {
+      response.writeHead(status, {'Content-Type': type});
+      if (body && body.pipe) body.pipe(response);
+      else response.end(body);
     });
 }).listen(8000);
 
 async function notAllowed(request) {
   return {
     status: 405,
-    body: `Method ${request.method} not allowed.`
+    body: `Method ${request.method} not allowed.`,
   };
 }
 
-var {parse} = require("url");
-var {resolve, sep} = require("path");
+var {parse} = require('url');
+var {resolve, sep} = require('path');
 
 var baseDirectory = process.cwd();
 
 function urlPath(url) {
   let {pathname} = parse(url);
   let path = resolve(decodeURIComponent(pathname).slice(1));
-  if (path != baseDirectory &&
-      !path.startsWith(baseDirectory + sep)) {
-    throw {status: 403, body: "Forbidden"};
+  if (path != baseDirectory && !path.startsWith(baseDirectory + sep)) {
+    throw {status: 403, body: 'Forbidden'};
   }
   return path;
 }
 
-const {createReadStream} = require("fs");
-const {stat, readdir} = require("fs").promises;
-const mime = require("mime");
+const {createReadStream} = require('fs');
+const {stat, readdir} = require('fs').promises;
+const mime = require('mime');
 
-methods.GET = async function(request) {
+methods.GET = async function (request) {
   let path = urlPath(request.url);
   let stats;
   try {
     stats = await stat(path);
   } catch (error) {
-    if (error.code != "ENOENT") throw error;
-    else return {status: 404, body: "File not found"};
+    if (error.code != 'ENOENT') throw error;
+    else return {status: 404, body: 'File not found'};
   }
   if (stats.isDirectory()) {
-    return {body: (await readdir(path)).join("\n")};
+    return {body: (await readdir(path)).join('\n')};
   } else {
-    return {body: createReadStream(path),
-            type: mime.getType(path)};
+    return {body: createReadStream(path), type: mime.getType(path)};
   }
 };
 
-const {rmdir, unlink} = require("fs").promises;
+const {rmdir, unlink} = require('fs').promises;
 
-methods.DELETE = async function(request) {
+methods.DELETE = async function (request) {
   let path = urlPath(request.url);
   let stats;
   try {
     stats = await stat(path);
   } catch (error) {
-    if (error.code != "ENOENT") throw error;
+    if (error.code != 'ENOENT') throw error;
     else return {status: 204};
   }
   if (stats.isDirectory()) await rmdir(path);
@@ -75,35 +73,35 @@ methods.DELETE = async function(request) {
   return {status: 204};
 };
 
-const {createWriteStream} = require("fs");
+const {createWriteStream} = require('fs');
 
 function pipeStream(from, to) {
   return new Promise((resolve, reject) => {
-    from.on("error", reject);
-    to.on("error", reject);
-    to.on("finish", resolve);
+    from.on('error', reject);
+    to.on('error', reject);
+    to.on('finish', resolve);
     from.pipe(to);
   });
 }
 
-methods.PUT = async function(request) {
+methods.PUT = async function (request) {
   let path = urlPath(request.url);
   await pipeStream(request, createWriteStream(path));
   return {status: 204};
 };
 
-const {mkdir} = require("fs").promises;
+const {mkdir} = require('fs').promises;
 
-methods.MKCOL = async function(request) {
+methods.MKCOL = async function (request) {
   let path = urlPath(request.url);
   let stats;
   try {
     stats = await stat(path);
   } catch (error) {
-    if (error.code != "ENOENT") throw error;
+    if (error.code != 'ENOENT') throw error;
     await mkdir(path);
     return {status: 204};
   }
   if (stats.isDirectory()) return {status: 204};
-  else return {status: 400, body: "Not a directory"};
+  else return {status: 400, body: 'Not a directory'};
 };
