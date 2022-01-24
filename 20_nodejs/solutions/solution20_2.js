bbc6bafa20959ef596d8bbe5d35897b03763323c
felixwurst <felix.wurst@gmail.com>
2022-01-24 21:22:47 +0100

    task solved

--- 20_nodejs/solutions/solution20_2.js
+++ 20_nodejs/solutions/solution20_2.js
@@ -3,3 +3,118 @@
 // Though the DELETE method in our file server is able to delete directories (using rmdir), the server currently does not provide any way to create a directory.
 
 // Add support for the MKCOL method (“make collection”), which should create a directory by calling mkdir from the fs module. MKCOL is not a widely used HTTP method, but it does exist for this same purpose in the WebDAV standard, which specifies a set of conventions on top of HTTP that make it suitable for creating documents.
+
+/* ************************************************** already existing code ************************************************** */
+
+const {createServer} = require('http');
+
+const methods = Object.create(null);
+
+createServer((request, response) => {
+  let handler = methods[request.method] || notAllowed;
+  handler(request)
+    .catch(error => {
+      if (error.status != null) return error;
+      return {body: String(error), status: 500};
+    })
+    .then(({body, status = 200, type = 'text/plain'}) => {
+      response.writeHead(status, {'Content-Type': type});
+      if (body && body.pipe) body.pipe(response);
+      else response.end(body);
+    });
+}).listen(8000);
+
+async function notAllowed(request) {
+  return {
+    status: 405,
+    body: `Method ${request.method} not allowed.`,
+  };
+}
+
+var {parse} = require('url');
+var {resolve, sep} = require('path');
+
+var baseDirectory = process.cwd();
+
+function urlPath(url) {
+  let {pathname} = parse(url);
+  let path = resolve(decodeURIComponent(pathname).slice(1));
+  if (path != baseDirectory && !path.startsWith(baseDirectory + sep)) {
+    throw {status: 403, body: 'Forbidden'};
+  }
+  return path;
+}
+
+const {createReadStream} = require('fs');
+const {stat, readdir} = require('fs').promises;
+const mime = require('mime');
+
+methods.GET = async function (request) {
+  let path = urlPath(request.url);
+  let stats;
+  try {
+    stats = await stat(path);
+  } catch (error) {
+    if (error.code != 'ENOENT') throw error;
+    else return {status: 404, body: 'File not found'};
+  }
+  if (stats.isDirectory()) {
+    return {body: (await readdir(path)).join('\n')};
+  } else {
+    return {body: createReadStream(path), type: mime.getType(path)};
+  }
+};
+
+const {rmdir, unlink} = require('fs').promises;
+
+methods.DELETE = async function (request) {
+  let path = urlPath(request.url);
+  let stats;
+  try {
+    stats = await stat(path);
+  } catch (error) {
+    if (error.code != 'ENOENT') throw error;
+    else return {status: 204};
+  }
+  if (stats.isDirectory()) await rmdir(path);
+  else await unlink(path);
+  return {status: 204};
+};
+
+const {createWriteStream} = require('fs');
+
+function pipeStream(from, to) {
+  return new Promise((resolve, reject) => {
+    from.on('error', reject);
+    to.on('error', reject);
+    to.on('finish', resolve);
+    from.pipe(to);
+  });
+}
+
+methods.PUT = async function (request) {
+  let path = urlPath(request.url);
+  await pipeStream(request, createWriteStream(path));
+  return {status: 204};
+};
+
+/* ************************************************** newly added code ************************************************** */
+
+const {mkdir} = require('fs').promises;
+
+methods.MKCOL = async function (request) {
+  let path = urlPath(request.url);
+  let stats;
+  try {
+    stats = await stat(path);
+  } catch (error) {
+    // directory/file does not exist
+    if (error.code != 'ENOENT') throw error;
+    await mkdir(path);
+    return {status: 204};
+  }
+  // directory exists
+  if (stats.isDirectory()) return {status: 204};
+  // file exists
+  else return {status: 400, body: 'Not a directory'};
+};
