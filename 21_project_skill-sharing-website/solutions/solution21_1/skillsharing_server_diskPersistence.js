df1822b484e09cb7db5535df6ab49aee5c591904
felixwurst <felix.wurst@gmail.com>
2022-03-28 13:18:55 +0200

    task definitions added

--- 21_project_skill-sharing-website/solutions/solution21_1/skillsharing_server_diskPersistence.js
+++ 21_project_skill-sharing-website/solutions/solution21_1/skillsharing_server_diskPersistence.js
@@ -1,3 +1,9 @@
+// Disk persistence
+
+// The skill-sharing server keeps its data purely in memory. This means that when it crashes or is restarted for any reason, all talks and comments are lost.
+
+// Extend the server so that it stores the talk data to disk and automatically reloads the data when it is restarted. Do not worry about efficiency—do the simplest thing that works.
+
 // This is a redifinition of the skillsharing_server component in this chapter
 
 /* **************************************** already existing code **************************************** */
